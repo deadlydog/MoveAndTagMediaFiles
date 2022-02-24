32e1e6c2b0df4c7177570c9ec648b9078f86d47c
deadlydog <deadlydog@hotmail.com>
2022-02-24 14:30:11 -0600

    Add Inverse Boolean Converter

---
+++ src/MoveAndTagMediaFilesWpfApp/Views/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MoveAndTagMediaFilesWpfApp.Views.Converters;
+
+public class InverseBooleanConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (targetType != typeof(bool))
+			throw new InvalidOperationException("The target must be a boolean");
+
+		return !(bool)value;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotSupportedException();
+	}
+}
